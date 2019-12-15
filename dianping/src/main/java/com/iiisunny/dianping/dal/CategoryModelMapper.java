52490ecf2ef5d2859a7626bbd3401148b65a04f1
sunny <422775690@qq.com>
2019-12-15 22:54:03 +0800

    update

---
+++ dianping/src/main/java/com/iiisunny/dianping/dal/CategoryModelMapper.java
@@ -0,0 +1,22 @@
+package com.iiisunny.dianping.dal;
+
+import com.iiisunny.dianping.model.CategoryModel;
+
+import java.util.List;
+
+public interface CategoryModelMapper {
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(CategoryModel record);
+
+    int insertSelective(CategoryModel record);
+
+    List<CategoryModel> selectAll();
+
+    CategoryModel selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(CategoryModel record);
+
+    int updateByPrimaryKey(CategoryModel record);
+}
